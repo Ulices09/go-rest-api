fbba4ec243b812c23fee354315005e1e4a04bc6c
Ulices Meléndez Acosta <ulices009@hotmail.com>
2021-08-30 13:37:10 -0500

    add tests to post service

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.17
 require (
 	github.com/go-playground/validator/v10 v10.9.0
 	github.com/labstack/echo/v4 v4.5.0
+	github.com/stretchr/testify v1.7.0
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/gorm v1.21.14
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
@@ -20,6 +22,8 @@ require (
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/mattn/go-sqlite3 v1.14.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
@@ -27,4 +31,5 @@ require (
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ service/post_service_test.go
@@ -0,0 +1,105 @@
+package service_test
+
+import (
+	"go-rest-api/entity"
+	"go-rest-api/service"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+/*
+  Test objects
+*/
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (m *MockRepository) FindAll() ([]entity.Post, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.([]entity.Post), args.Error(1)
+}
+
+func (m *MockRepository) FindById(id int) (*entity.Post, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
+
+func (m *MockRepository) Create(post *entity.Post) (*entity.Post, error) {
+	args := m.Called()
+	result := args.Get(0)
+	return result.(*entity.Post), args.Error(1)
+}
+
+/*
+  Test data
+*/
+
+var post1 = entity.Post{
+	Title: "My first post",
+	Text:  "This is my first post",
+	Model: entity.Model{
+		ID:        1,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	},
+}
+
+var post2 = entity.Post{
+	Title: "My second post",
+	Text:  "This is my second post",
+	Model: entity.Model{
+		ID:        2,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	},
+}
+
+var postsData = []entity.Post{post1, post2}
+
+/*
+  Test functions
+*/
+func TestGetAll(t *testing.T) {
+	mockRepo := new(MockRepository)
+	mockRepo.On("FindAll").Return(postsData, nil)
+
+	service := service.NewPostService(mockRepo)
+	posts, err := service.GetAll()
+
+	mockRepo.AssertExpectations(t)
+	assert.Equal(t, 2, len(posts))
+	assert.Nil(t, err)
+}
+
+func TestGetById(t *testing.T) {
+	mockRepo := new(MockRepository)
+	mockRepo.On("FindById").Return(&post1, nil)
+
+	service := service.NewPostService(mockRepo)
+	post, err := service.GetById(1)
+
+	mockRepo.AssertExpectations(t)
+	assert.Equal(t, post1.ID, post.ID)
+	assert.Equal(t, post1.Title, post.Title)
+	assert.Equal(t, post1.Text, post.Text)
+	assert.Nil(t, err)
+}
+
+func TestCreate(t *testing.T) {
+	mockRepo := new(MockRepository)
+	mockRepo.On("Create").Return(&post1, nil)
+
+	service := service.NewPostService(mockRepo)
+	post, err := service.Create(&post1)
+
+	mockRepo.AssertExpectations(t)
+	assert.Equal(t, post1.Title, post.Title)
+	assert.Equal(t, post1.Text, post.Text)
+	assert.Nil(t, err)
+}
