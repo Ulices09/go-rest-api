ba0b9b3ea473b6ae30c3c65896d11dfe0ff53289
Ulices Meléndez Acosta <ulices009@hotmail.com>
2021-09-24 00:01:23 -0500

    remove unnecessary pointers in middleware

--- internal/interface/http/middleware.go
+++ internal/interface/http/middleware.go
@@ -17,32 +17,32 @@ func InitMiddlware(config config.Config) CustomMiddleware {
 	return CustomMiddleware{config}
 }
 
-func (*CustomMiddleware) Logger() echo.MiddlewareFunc {
+func (CustomMiddleware) Logger() echo.MiddlewareFunc {
 	return middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${method}, uri=${uri}, status=${status}\n",
 	})
 }
 
-func (m *CustomMiddleware) CORS() echo.MiddlewareFunc {
+func (m CustomMiddleware) CORS() echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     m.config.Host.AllowOrigins,
 		AllowCredentials: true,
 	})
 }
 
-func (*CustomMiddleware) CSRF() echo.MiddlewareFunc {
+func (CustomMiddleware) CSRF() echo.MiddlewareFunc {
 	return middleware.CSRF()
 }
 
-func (*CustomMiddleware) Recover() echo.MiddlewareFunc {
+func (CustomMiddleware) Recover() echo.MiddlewareFunc {
 	return middleware.Recover()
 }
 
-func (*CustomMiddleware) Secure() echo.MiddlewareFunc {
+func (CustomMiddleware) Secure() echo.MiddlewareFunc {
 	return middleware.Secure()
 }
 
-func (m *CustomMiddleware) Auth() echo.MiddlewareFunc {
+func (m CustomMiddleware) Auth() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			tokenCookie, err := GetSessionCookie(c)
